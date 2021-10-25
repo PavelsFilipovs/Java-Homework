cd13313d8a0bd941e3a7766d7b20c7a88f2f4154
Pāvels <pavels.filipovs.oop@gmail.com>
2021-10-25 17:05:54 +0300

    make Scanner ask and in put

--- src/homework_2/homework_2.java
+++ src/homework_2/homework_2.java
@@ -1,10 +1,19 @@
 package homework_2;
 
+import java.util.Scanner;
+
 public class homework_2 {
 
 	public static void main(String[] args) {
-		System.out.println("HI");
+		
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Ievadiet ciparu pa X asi: ");
+		int x = in.nextInt();
 
+		System.out.print("Ievadiet ciparu pa Y asi: ");
+		int y = in.nextInt();
 	}
 
 }

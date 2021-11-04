9bec617447ee0e97ba570f707250fd41b0c4b9a9
Pāvels <pavels.filipovs.oop@gmail.com>
2021-11-04 18:48:45 +0200

    Add massive

--- src/homework_4/SortArray.java
+++ src/homework_4/SortArray.java
@@ -1,9 +1,20 @@
 package homework_4;
 
+import java.util.Scanner;
+
 public class SortArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int arrLenght = 0;
+		Scanner scan = new Scanner(System.in);
+		int[] arr = new int[arrLenght];
+		
+		
+		System.out.print("Ievadiet masîva lielumu no 20 lîdz 40: ");
+		arrLenght = scan.nextInt();
+		
+		System.out.println(arr.length);
+		
 
 	}
 
